2980882ee3eb21b24a2e72d688ecd579edc9aefe
Vismura <natali-ivleva@list.ru>
2022-11-17 01:34:20 +0700

    1hw

--- program/HW1/Program.cs
+++ program/HW1/Program.cs
@@ -1,2 +1,23 @@
-﻿Console.Write("Введите два числа ");
-string UserNumbers = Console.ReadLine();
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите первое число ");
+        double num1 = Convert.ToInt64(Console.ReadLine());
+        Console.WriteLine("Введите второе число ");
+        double num2 = Convert.ToInt64(Console.ReadLine());
+
+        if (num1 > num2)
+        {
+            Console.WriteLine(num1);
+        }
+        else if (num1 < num2)
+        {
+            Console.WriteLine(num2);
+        }
+        else
+        {
+            Console.WriteLine("Оба числа равны");
+        }
+    }
+}
