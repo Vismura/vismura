0a7e6acc1e4ebc40cd41bf3c0adfc30777e608aa
Vismura <natali-ivleva@list.ru>
2022-12-01 20:23:21 +0700

    123

---
+++ seminar4/hw2/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("введите число");
+        int i = Convert.ToInt32(Console.ReadLine());
+        int sum = 0;
+
+        if (i > 0)
+        {
+            while (i > 0)
+            {
+                int num = i % 10;
+                i = i / 10;
+                sum = sum + num;
+            }
+            Console.Write("сумма всех цифр в числе равна: " + sum);
+        }
+
+        if (i < 0)
+        {
+            Console.WriteLine("Введено неправильное число");
+        }
+    }
+}
---
+++ seminar4/hw3/Program.cs
@@ -0,0 +1,17 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] numbers = new int[8];
+        Console.Write("[");
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = new Random().Next(0, 999);
+            if (i < numbers.Length - 1) Console.Write($"{numbers[i]}, ");
+
+            else Console.Write(numbers[i]);
+        }
+        Console.Write("]");
+    }
+}
