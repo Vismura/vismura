a00d52159318108f8f1d29902c42c55d0592e25a
Илья Пальчик <vismura@MacBook-Air-Ila.local>
2022-12-25 21:59:29 +0900

    11

--- seminar9/hw3/Program.cs
+++ seminar9/hw3/Program.cs
@@ -1,18 +1,24 @@
-﻿Console.WriteLine("Введите два положительных числа: M и N.");
-int m = InputInt("Введите M: ");
-int n = InputInt("Введите N: ");
-Console.WriteLine($"A({m}, {n}) = {Akkerman(m, n)}");
-int InputInt(string output)
+﻿internal class Program
 {
-    Console.Write(output);
-    return int.Parse(Console.ReadLine());
-}
-int Akkerman(int m, int n)
-{
-    if (m == 0)
-        return n + 1;
-    if (m > 0 && n == 0)
-        return Akkerman(m - 1, 1);
-    else
-        return Akkerman(m - 1, Akkerman(m, n - 1));
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите два положительных числа: M и N.");
+        int m = InputInt("Введите M: ");
+        int n = InputInt("Введите N: ");
+        Console.WriteLine($"A({m}, {n}) = {Akkerman(m, n)}");
+        int InputInt(string output)
+        {
+            Console.Write(output);
+            return int.Parse(Console.ReadLine());
+        }
+        int Akkerman(int m, int n)
+        {
+            if (m == 0)
+                return n + 1;
+            if (m > 0 && n == 0)
+                return Akkerman(m - 1, 1);
+            else
+                return Akkerman(m - 1, Akkerman(m, n - 1));
+        }
+    }
 }
